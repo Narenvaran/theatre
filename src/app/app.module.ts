7fc3309cb441ac4aee5948627adb1a2929cfadc5
Ajay Mohan <ajay.mohan@intellectdesign.com>
2020-04-22 14:03:45 +0530

    Changes for adding ad banner carousel in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { SampleDateComponent } from './sample-date/sample-date.component';
+import { HomeAdCarouselComponent } from './home-ad-carousel/home-ad-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieCardComponent,
-    SampleDateComponent
+    SampleDateComponent,
+    HomeAdCarouselComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home-ad-carousel/home-ad-carousel.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit,ViewChild, AfterViewInit } from '@angular/core';
+import { NgbCarousel,NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { AjaxService } from '../services/ajax.service';
+import { DatesService } from '../services/date.service';
+import { DateCommuteService } from '../services/datecommute.service';
+
+@Component({
+  selector: 'app-home-ad-carousel',
+  templateUrl: './home-ad-carousel.component.html',
+  styleUrls: ['./home-ad-carousel.component.css']
+})
+export class HomeAdCarouselComponent implements OnInit {
+
+ @ViewChild('myCarousel', {static: false}) myCarousel: NgbCarousel;
+ carouselInput:string[];
+  
+  constructor(private config: NgbCarouselConfig,
+              private ajaxService:AjaxService,
+              private dateService:DatesService,
+              private dateCommuteService:DateCommuteService) {
+    this.config.interval = 5000;
+  }
+
+  ngOnInit() {
+    this.dateCommuteService.selectedDate.subscribe(
+      selectedDate => 
+      this.renderHomeAds(selectedDate)
+      );
+  }
+
+  ngAfterViewInit(){
+    console.log(this.myCarousel);
+  }
+
+  private renderHomeAds(selectedDate:string){
+    this.ajaxService.getMethod('https://devitheater-4c18c.firebaseio.com/dates.json').subscribe(
+      adsData => {
+        let index = this.dateService.getDaysBetween(selectedDate);
+        this.carouselInput = <string[]>adsData[index].ads;
+      },
+      error => {
+        console.log("this is the error from carousel ajax call ->"+error)
+      }
+    );
+  }
+
+  goToFirstCard(){
+    this.myCarousel.select("0");
+  }
+
+}
